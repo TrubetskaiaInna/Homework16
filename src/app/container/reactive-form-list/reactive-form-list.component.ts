c50b20958cc98939e81b06a1fe30ff1a1934ee41
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2020-02-13 21:34:40 +0200

    added valid reactive form

--- src/app/container/reactive-form-list/reactive-form-list.component.ts
+++ src/app/container/reactive-form-list/reactive-form-list.component.ts
@@ -1,38 +1,41 @@
 import {Component} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, Validators, FormGroup, Validator} from '@angular/forms';
 
 @Component({
   templateUrl: './reactive-form-list.html',
   styleUrls: ['./reactive-form-list.scss']
 })
 
+
 export class ReactiveFormListComponent {
   constructor(private fb: FormBuilder) {
   }
 
   profileForm = this.fb.group({
-    firstName: [''],
-    lastName: [''],
-    userName: [''],
-    email: [''],
-    password: [''],
-    confirmPassword: [''],
-    region: [''],
+    firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
+    lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
+    userName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(10)]],
+    confirmPassword: ['', [Validators.required]],
+    region: ['', Validators.required],
     sex: [''],
     emailPermission: false,
     additionalInfo: ['']
   });
 
-  onSubmit() {
+
+
+  onSubmit(group: FormGroup) {
     console.log(this.profileForm.value);
     this.profileForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      userName: [''],
-      email: [''],
-      password: [''],
+      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
+      lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
+      userName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(10)]],
       confirmPassword: [''],
-      region: [''],
+      region: ['', Validators.required],
       sex: [''],
       emailPermission: false,
       additionalInfo: ['']
