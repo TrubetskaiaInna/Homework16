6a802bd56302576d32c9830cd21365385918efff
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2020-02-14 09:13:44 +0200

    fixed valid reactive form

--- src/app/container/reactive-form-list/reactive-form-list.component.ts
+++ src/app/container/reactive-form-list/reactive-form-list.component.ts
@@ -15,18 +15,50 @@ export class ReactiveFormListComponent {
     firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
     lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
     userName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
-    email: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.pattern('^([A-Za-z0-9_\\-.])+@([A-Za-z0-9_\\-.])+\\.([A-Za-z]{2,4})$')]],
     password: ['', [Validators.required, Validators.minLength(10)]],
-    confirmPassword: ['', [Validators.required]],
+    confirmPassword: ['', Validators.required],
     region: ['', Validators.required],
     sex: [''],
     emailPermission: false,
     additionalInfo: ['']
   });
 
+  get firstName() {
+    return this.profileForm.get('firstName');
+  }
 
+  get lastName() {
+    return this.profileForm.get('lastName');
+  }
 
-  onSubmit(group: FormGroup) {
+  get userName() {
+    return this.profileForm.get('userName');
+  }
+
+  get email() {
+    return this.profileForm.get('email');
+  }
+
+  get password() {
+    return this.profileForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.profileForm.get('confirmPassword');
+  }
+
+  get region() {
+    return this.profileForm.get('region');
+  }
+
+  // checkPasswords(group: FormGroup) {
+  //   const pass = group.controls.password.value;
+  //   const confirmPass = group.controls.confirmPassword.value;
+  //   return pass === confirmPass ? null : {not: true};
+  // }
+
+  onSubmit() {
     console.log(this.profileForm.value);
     this.profileForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
@@ -34,7 +66,7 @@ export class ReactiveFormListComponent {
       userName: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$')]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(10)]],
-      confirmPassword: [''],
+      confirmPassword: ['', Validators.required],
       region: ['', Validators.required],
       sex: [''],
       emailPermission: false,
