ba92de99a0828c233ab03f6b1afb21330924a263
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2020-02-20 21:15:26 +0200

    fixed app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {PageNotFoundModule} from './container/page-not-found/page-not-found.module';
 import {PageNotFoundComponent} from './container/page-not-found/page-not-found.component';
-import {ReactiveFormListModule} from './container/reactive-form-list/reactive-form-list.module';
 import {ReactiveFormListComponent} from './container/reactive-form-list/reactive-form-list.component';
-import {TemplateDrivenFormModule} from './container/template-driven-form-list/template-driven-form.module';
 import {TemplateDrivenFormComponent} from './container/template-driven-form-list/template-driven-form.component';
 
 const routes: Routes = [
@@ -15,11 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),
-    PageNotFoundModule,
-    ReactiveFormListModule,
-    TemplateDrivenFormModule
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PageNotFoundModule} from './container/page-not-found/page-not-found.module';
+import {ReactiveFormListModule} from './container/reactive-form-list/reactive-form-list.module';
+import {TemplateDrivenFormModule} from './container/template-driven-form-list/template-driven-form.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PageNotFoundModule,
+    ReactiveFormListModule,
+    TemplateDrivenFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
